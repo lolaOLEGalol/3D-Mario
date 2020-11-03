8d242eed9145ff396a750cf23c6e40b2e7c43238
oleg <experementator@gmail.com>
2020-11-03 04:55:50 +0300

    0.3 barrier and spawn mario

---
+++ Mario3D/mario3D/Assets/Prefabs/player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private float speed = 0.23f;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.left * speed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * speed);
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.back * speed);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.forward * speed);
+        }
+    }
+}
