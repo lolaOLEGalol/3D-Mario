7a5d141be492fcc3387ff0051f961f2f95a39004
oleg <experementator@gmail.com>
2020-11-04 01:50:19 +0300

    0.4 Code for Mario

--- Mario3D/mario3D/Assets/Prefabs/player.cs
+++ Mario3D/mario3D/Assets/Prefabs/player.cs
@@ -5,12 +5,40 @@ using UnityEngine;
 public class player : MonoBehaviour
 {
     private float speed = 0.23f;
+    public int lvl_mario = 1;
+    private float jump = 5f;
+    public GameObject stone;
+    public GameObject question;
 
-    void Start()
+
+    private void OnTriggerStay (Collider other)
     {
-        
+
+        if (other.tag == "down" && Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.Translate(Vector3.up * jump);
+        }
     }
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "boost" && lvl_mario < 3)
+        {
+            lvl_mario++;
+        }
+
+        if (other.tag == "enemy" && lvl_mario > 0)
+        {
+            lvl_mario--;
+        }
+
+        if (other.tag == "hole")
+        {
+            lvl_mario = 0;
+        }
+    }
+
+    void TranslateMario()
     {
         if (Input.GetKey(KeyCode.D))
         {
@@ -27,6 +55,19 @@ public class player : MonoBehaviour
         else if (Input.GetKey(KeyCode.S))
         {
             transform.Translate(Vector3.forward * speed);
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        TranslateMario();
+        if (lvl_mario == 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
