c970540a7bc8818d342a8cfe611042c7fa3aef9e
oleg <experementator@gmail.com>
2020-12-11 14:33:37 +0300

    ALPHAVERSION

---
+++ mario3D/Assets/Scripts/destroyMoney.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyMoney : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "mario")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ mario3D/Assets/Scripts/enimiesRL.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enimiesRL : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+    private bool visibleMario = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "invert")
+        {
+            speed *= -1;
+        }
+
+        if (other.tag == "Sphere")
+        {
+            visibleMario = true;
+        }
+       
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        
+    }
+    void Update()
+    {
+        if (visibleMario)
+        {
+            Vector3 move = new Vector3(1, 0, 0);
+            transform.Translate(move * Time.deltaTime * speed);
+        }
+        
+    }
+}
